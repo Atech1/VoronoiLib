80dc258213bed9d1737e4c5e7247d6bfbb7d9921
Atech1 <myself.therest@gmail.com>
2018-06-22 21:20:48 -0400

    made the Point class Iequatable

--- VoronoiLib/Structures/Point.cs
+++ VoronoiLib/Structures/Point.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using System;
 namespace VoronoiLib.Structures
 {
-    public class Point
+    public class Point : IEquatable<Point>
     {
         public double X { get; }
         public double Y { get; }
@@ -15,6 +15,29 @@ namespace VoronoiLib.Structures
         public override string ToString()
         {
             return $"{this.X}, {this.Y}";
+        }
+
+        public bool Equals(Point other)
+        {
+            if(other == null)
+            {
+                return false;
+            }
+
+            return this.X == other.X && this.Y == other.Y ? true : false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(Point) ? this.Equals((Point) obj) : false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = (hash * 7) + this.X.GetHashCode();
+            hash = (hash * 7) + this.Y.GetHashCode();
+            return hash;
         }
     }
 }
