400962d84181c622ee465f1dcccb3b6590ef8536
Atech1 <myself.therest@gmail.com>
2018-06-22 20:18:32 -0400

    added a generic RBT library instead of the custom generic code

---
+++ VoronoiLib/Structures/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VoronoiLib.Structures
+{
+    public class Point
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        internal Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.X}, {this.Y}";
+        }
+    }
+}
